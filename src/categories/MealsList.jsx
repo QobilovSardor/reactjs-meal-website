57563d4d0a9f0a7dcd3d2606489e8d798ecf0287
SardorproDev <sardorqobilov555@gmail.com>
2023-02-15 17:55:16 +0500

    90% project finished

--- src/categories/MealsList.jsx
+++ src/categories/MealsList.jsx
@@ -1,39 +1,86 @@
-import { useParams } from "react-router-dom";
-import {Card, CardBody, CardTitle, Button, CardSubtitle, CardText } from 'reactstrap';
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {Card, CardBody, CardTitle, Button, CardText, Table } from 'reactstrap';
+import { getMealId } from "../config/api";
+import Loader from "../layouts/Loader";
 
-function MealsList(props) {
+function MealsList() {
   const {id} = useParams();
-  const {strMeal, strCategory, strArea, strInstructions, strMealThumb} = props;
+  const goBack = useNavigate();
+  const [recipe, setRecipe] = useState({});
+  const [showrecipe, setShowRepice] = useState(false);
+
+  const handlerShow = () => {
+    setShowRepice(!showrecipe)
+  }
+
+  useEffect(() => {
+    getMealId(id).then(data => setRecipe(data.meals[0]))
+  }, [])
+
 
   return (
     <div className="container my-5">
-      <Card
-        style={{
-          width: '18rem'
-        }}
-      >
-        <img
-          alt="Sample"
-          src={strMealThumb}
-        />
-        <CardBody>
-          <CardTitle tag="h5">
-            Card title
-          </CardTitle>
-          <CardSubtitle
-            className="mb-2 text-muted"
-            tag="h6"
-          >
-            Card subtitle
-          </CardSubtitle>
-          <CardText>
-            {strInstructions}
-          </CardText>
-          <Button>
-            Button
-          </Button>
-        </CardBody>
-      </Card>
+      <Button color="primary" onClick={() => goBack(-1)} className='mb-5'>
+        Go Back
+      </Button>
+      {!recipe.idMeal ? <Loader /> : (
+        <Card
+          style={{
+            width: '50%'
+          }}
+        >
+          <img
+            alt={recipe.strCategory}
+            src={recipe.strMealThumb}
+            style={{height: '30rem', objectFit: 'cover'}}
+          />
+          <CardBody>
+            <CardTitle tag="h5">
+              {recipe.strMeal}
+              <hr />
+            </CardTitle>
+            <CardTitle tag='h5'>
+              {recipe.strCategory}
+              <hr />
+            </CardTitle>
+            {recipe.strArea ? (
+              <CardTitle tag="h5">
+                {recipe.strArea}
+                <hr />
+              </CardTitle>
+            ): null}
+            <CardText>
+              {recipe.strInstructions}
+            </CardText>
+            <Button onClick={handlerShow} color='primary' className="my-3" >Show Recipe</Button>
+            {showrecipe ? (
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Ingredient</th>
+                    <th>Measure</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.keys(recipe).map((key, index) => {
+                    if(key.includes('Ingredient') && recipe[key]) {
+                      return (
+                        <tr key={index}>
+                          <td>{recipe[key]}</td>
+                          <td>{recipe[`strMeasure${key.slice(13)}`]}</td>
+                        </tr>
+                      )
+                    }
+                  })}
+                </tbody>
+              </Table>
+            ) : null}
+            <h4>Watch video</h4>
+            {recipe.strYoutube ? <iframe style={{width: '100%', height: '300px'}} src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(- 11)}`} title={id} /> : null}
+          </CardBody>
+        </Card>
+      )}
     </div>
   );
 }
--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,8 +1,8 @@
 import { API_URL } from "./config";
 
-const getMealId  = async (mealId) => {
-  const response = await fetch(API_URL + 'lookup.php?i' + mealId);
-  return await response.json()
+const getMealId = async (mealId) => {
+  const response = await fetch(API_URL + 'lookup.php?i=' + mealId)
+  return await response.json();
 }
 
 const getAllCategoris = async () => {
